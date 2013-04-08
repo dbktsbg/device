250e5a449e0cf5e65bddc65ce6c7f90bb18f43c3
david <dbkdev50@gmail.com>
2013-04-08 18:47:55 -0400

    WatchAcceleration

--- www/js/accelerometer.js
+++ www/js/accelerometer.js
@@ -1,20 +1,66 @@
 
+var watchID = null;
+var PreviousX = 0;
+var PreviousY = 0;
+var PreviousZ = 0;
+
 function InitializeAccelerometer() 
 {
-
+    startWatch();
 }
 
-function getCurrentAcceleration() 
+//function getCurrentAcceleration() 
+//{
+//    navigator.accelerometer.getCurrentAcceleration(onAccelerationSuccess, onAccelerationFail);
+//}
+
+//function onAccelerationSuccess(acceleration) 
+//{
+//    alert('Acceleration X: ' + acceleration.x + '<BR>' + 'Acceleration Y: ' + acceleration.y + '<BR>' + 'Acceleration Z: ' + acceleration.z + '<BR>');
+//}
+
+//function onAccelerationFail(message) 
+//{
+//    alert('Failed because: ' + message);
+//}
+
+function startWatch() 
 {
-    navigator.accelerometer.getCurrentAcceleration(onAccelerationSuccess, onAccelerationFail);
+    var options = { frequency: 500 };
+    watchID = navigator.accelerometer.watchAcceleration(onSuccess, onError, options);
 }
 
-function onAccelerationSuccess(acceleration) 
+function stopWatch() 
 {
-    alert('Acceleration X: ' + acceleration.x + '<BR>' + 'Acceleration Y: ' + acceleration.y + '<BR>' + 'Acceleration Z: ' + acceleration.z + '<BR>');
+    if (watchID) 
+    {
+        navigator.accelerometer.clearWatch(watchID);
+        watchID = null;
+    }
 }
 
-function onAccelerationFail(message) 
+function onSuccess(acceleration) 
 {
-    alert('Failed because: ' + message);
+    var element = document.getElementById('accelerometer');
+
+    if (PreviousX != acceleration.x) || (PreviousY != acceleration.y) || (PreviousZ != acceleration.z) 
+    {
+
+        // wake-up screen...
+
+        PreviousX = acceleration.x;
+        PreviousY = acceleration.y;
+        PreviousZ = acceleration.z;
+
+        element.innerHTML = 'Acceleration X: ' + acceleration.x + '<br />' +
+                                        'Acceleration Y: ' + acceleration.y + '<br />' +
+                                        'Acceleration Z: ' + acceleration.z + '<br />' +
+                                        'Timestamp: ' + acceleration.timestamp + '<br />';
+    }
 }
+
+function onError() 
+{
+    element.innerHTML = 'ERROR: ' + watchID + '<br />';
+}
+
